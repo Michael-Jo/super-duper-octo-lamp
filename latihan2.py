3ffe9576211bcfa0d84713cdc9c3f08f79098f48
Michael-Jo <michjo2008@gmail.com>
2024-02-03 10:49:23 +0700

    added desc

--- latihan2.py
+++ latihan2.py
@@ -1,7 +1,8 @@
 # Kumpul di moodle, by Michael Jonathan Sandy
-n = int(input("Masukan bilangan :"))
-while (10 >= n >= 15) or (20 >= n >= 25) or (35 >= n >= 40):
+n = int(input("Masukan bilangan diantara 10-15 atau 20-25 atau 35-40 :"))
+# while (10 > n > 15) or (20 > n > 25) or (35 > n > 40):
+while (n < 10) or (15 < n < 20) or (25 < n < 35) or (40 < n):
     print("Bilangan salah !")
-    n = int(input("Masukan bilangan :"))
+    n = int(input("Masukan bilangan diantara 10-15 atau 20-25 atau 35-40 :"))
 else:
     print("Bilangan benar")
